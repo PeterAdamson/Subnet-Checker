0aa670e7cfe07af17f8d0941984e3a9a415c44d3
peter adamson <peter.adamson@ibm.com>
2023-07-23 15:01:41 -0300

    fixed references in test file

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-import subnet_conflict_checker
+import subnet_inventory
 from subprocess import check_output
 
 
@@ -10,18 +10,18 @@ class TestFormatting(unittest.TestCase):
 
     def test_address_format_correct(self):
         correct_format = "10.0.0.0/24"
-        self.assertTrue(subnet_conflict_checker.check_address_format(cidr_string=correct_format))
+        self.assertTrue(subnet_inventory.check_address_format(cidr_string=correct_format))
 
     def test_address_format_incorrect(self):
         incorrect_format = "10.0.0.0"
-        self.assertFalse(subnet_conflict_checker.check_address_format(cidr_string=incorrect_format))
+        self.assertFalse(subnet_inventory.check_address_format(cidr_string=incorrect_format))
 
 
 # System Tests
 class TestSubnetConflictChecker(unittest.TestCase):
 
     def test_add_subnet(self):
-        out = check_output(["python3", "subnet_conflict_checker.py", "-l"]).decode()
+        out = check_output(["python3", "subnet_inventory.py", "-l"]).decode()
         self.assertIn("Displaying all subnets", out)
 
 
